a46df7e947c65660c507f5d610d64a100405daed
artemaminov <artemaminov@gmail.com>
2020-06-30 16:19:19 +0500

    refactoring to object

--- vendor/assets/javascripts/spree/backend/cropper.js
+++ vendor/assets/javascripts/spree/backend/cropper.js
@@ -1,100 +1,102 @@
-let devices = [
-    {
-        desktop: {
-            coords: {},
-            dimensions: {
-                width: 1024,
-                height: 400
+let Croom = function() {
+    this.devices = [
+        {
+            desktop: {
+                coords: {},
+                dimensions: {
+                    width: 1024,
+                    height: 400
+                }
             }
-        }
-    },
-    {
-        tablet_land: {
-            coords: {},
-            dimensions: {
-                width: 600,
-                height: 300
+        },
+        {
+            tablet_land: {
+                coords: {},
+                dimensions: {
+                    width: 600,
+                    height: 300
+                }
             }
-        }
-    },
-    {
-        tablet_port: {
-            coords: {},
-            dimensions: {
-                width: 300,
-                height: 600
+        },
+        {
+            tablet_port: {
+                coords: {},
+                dimensions: {
+                    width: 300,
+                    height: 600
+                }
             }
-        }
-    },
-    {
-        mobile: {
-            coords: {},
-            dimensions: {
-                width: 200,
-                height: 400
+        },
+        {
+            mobile: {
+                coords: {},
+                dimensions: {
+                    width: 200,
+                    height: 400
+                }
             }
-        }
-    },
-];
+        },
+    ];
+}
 
-let initTargets = function() {
-    for (let target of devices) {
-        $('#targets').append(`<div id="target-${getDeviceName(target)}"></div>`);
-        canvasCopy(target);
-        initCrop(target);
+Croom.prototype.initTargets = function() {
+    for (let target of this.devices) {
+        $('#targets').append(`<div id="target-${this.getDeviceName(target)}"></div>`);
+        this.canvasCopy(target);
+        this.initCrop(target);
     }
 }
 
-let initCrop = function(target) {
-    $('#previews').append(`<div id="preview-${getDeviceName(target)}"></div>`);
-    let dimensions = getDevice(target).dimensions;
+Croom.prototype.initCrop = function(target) {
+    $('#previews').append(`<div id="preview-${this.getDeviceName(target)}"></div>`);
+    let dimensions = this.getDevice(target).dimensions;
     console.log(dimensions);
-    $(`#${getDeviceName(target)}-canvas`).rcrop({
+    $(`#${this.getDeviceName(target)}-canvas`).rcrop({
         minSize: [dimensions.width, dimensions.height],
         preserveAspectRatio: true,
         inputs: true,
         preview: {
             display: true,
             size: [dimensions.width, dimensions.height],
-            wrapper: `#preview-${getDeviceName(target)}`,
+            wrapper: `#preview-${this.getDeviceName(target)}`,
             inputs: true
         }
     });
 }
 
-let getDeviceName = function(device) {
+Croom.prototype.getDeviceName = function(device) {
     return Object.keys(device)[0];
 }
 
-let getDevice = function(target) {
-    return target[getDeviceName(target)];
+Croom.prototype.getDevice = function(target) {
+    return target[this.getDeviceName(target)];
 }
 
-let canvasCopy = function(target) {
+Croom.prototype.canvasCopy = function(target) {
     let source = document.getElementById(`source`);
-    let deviceName = getDeviceName(target);
+    let deviceName = this.getDeviceName(target);
     let canvasesContainer = document.getElementById(`target-${deviceName}`);
     let canvas = source.cloneNode();
     canvas.id = `${deviceName}-canvas`;
     canvasesContainer.appendChild(canvas);
 }
 
-let getCoords = function(target) {
-    return $(`#${getDeviceName(target)}-canvas`).rcrop('getValues');
+Croom.prototype.getCoords = function(target) {
+    return $(`#${this.getDeviceName(target)}-canvas`).rcrop('getValues');
 }
 
-$('#values').click(function() {
-    let text = '';
-    let coords = {};
-    for (let target of devices) {
-        coords = getCoords(target);
-        text += `${ getDeviceName(target) }: ${ coords.width }x${ coords.height }+${ coords.x }+${ coords.y } | `;
-        getDevice(target).coords = coords;
-    }
-    $('#results').text(`${text}`);
-    console.log(JSON.stringify(devices));
-});
-
 $(function() {
-    initTargets();
+    let croom = new Croom();
+    croom.initTargets();
+    $('#values').click(function() {
+        let text = '';
+        let coords = {};
+        for (let target of croom.devices) {
+            coords = croom.getCoords(target);
+            text += `${croom.getDeviceName(target) }: ${ coords.width }x${ coords.height }+${ coords.x }+${ coords.y } | `;
+            croom.getDevice(target).coords = coords;
+        }
+        $('#results').text(`${text}`);
+        console.log(JSON.stringify(croom.devices));
+    });
 });

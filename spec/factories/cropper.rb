748fe8fc276f757980a3b5a773a8ccc40cb52109
artemaminov <artemaminov@gmail.com>
2020-07-07 16:24:59 +0500

    add name, cropped_image to cropper_spec

--- spec/factories/cropper.rb
+++ spec/factories/cropper.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :cropper, class: 'Spree::Cropper' do
+    name { FFaker::Animal.common_name }
+    cropped_image { create :cropped_image }
     width { FFaker::Random.rand 2000 }
     height { FFaker::Random.rand 2000 }
     x { FFaker::Random.rand 2000 }

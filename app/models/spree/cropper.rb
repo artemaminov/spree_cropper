5c028a8d077f72033dfc667d86e2fa3cf4af706b
artemaminov <artemaminov@gmail.com>
2020-07-08 13:34:47 +0500

    pass dimensions to CropperImage

--- app/models/spree/cropper.rb
+++ app/models/spree/cropper.rb
@@ -1,9 +1,12 @@
 module Spree
   class Cropper < Spree::Base
-    # attr_accessor :dimensions
+    attr_accessor :dimensions
     belongs_to :cropped_image, required: true
 
     validates_associated :cropped_image
+    validates_presence_of :name, :width, :height, :x, :y, :cmd
+
+    before_validation :parse_dimensions
 
     def process(image, cropping_areas)
       cropping_areas.each do |area|
@@ -11,7 +14,10 @@ module Spree
       end
     end
 
-    def generate_cmd
+    def parse_dimensions
+      dimensions_hash = JSON.parse(dimensions)
+      cropper_params = dimensions_hash.slice('name', 'width', 'height', 'x', 'y')
+      self.assign_attributes(cropper_params)
       self.cmd = "#{ width }x#{ height }+#{ x }+#{ y }"
     end
 

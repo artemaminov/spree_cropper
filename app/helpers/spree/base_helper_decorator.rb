00aa3a8cd2dce131cc8deaf85d1eec8b4c794f65
artemaminov <artemaminov@gmail.com>
2020-07-22 20:20:56 +0500

    add cropped_image_tage helper method

---
+++ app/helpers/spree/base_helper_decorator.rb
@@ -0,0 +1,15 @@
+Spree::BaseHelper.module_eval do
+  def cropped_image_tag(image, image_class = "")
+    content_tag :picture do
+      images = Spree::CropperDevice.dimensions.map { |device, dimensions|
+        unless image.blank?
+          content_tag :source, "", { media: "(max-width: #{ dimensions[:width] }px)", srcset: main_app.url_for(image.attachment.variant(crop: image.for(device)))}
+        end
+      }
+      unless image.blank?
+        images << [image_tag(main_app.url_for(image.attachment.variant(crop: image.for('desktop'))), class: image_class)]
+      end
+      safe_join(images, "\n")
+    end
+  end
+end

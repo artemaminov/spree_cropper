dbd8396e55905305a03ab27069b8bdbf9b989501
artemaminov <artemaminov@gmail.com>
2020-07-25 18:53:49 +0500

    add #cropper_image_tag helper

---
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -0,0 +1,15 @@
+Spree::Admin::BaseHelper.module_eval do
+  def cropper_image_tag(f, resource)
+    resource.build_cropped_image if resource.cropped_image.blank?
+    output = f.fields_for :cropped_image, resource.cropped_image do |cropped_image_fields|
+      cropped_image_fields.label :attachment
+      cropped_image_fields.file_field :attachment
+      cropped_image_fields.check_box :_destroy
+      unless resource.cropped_image.new_record?
+        render partial: '/spree/admin/shared/cropper_form', locals: { resource: resource.cropped_image, cropped_image_fields: cropped_image_fields }
+      end
+    end
+    byebug
+    output.html_safe
+  end
+end
---
+++ app/helpers/spree/cropper_helper.rb
@@ -0,0 +1,16 @@
+module Spree
+  module CropperHelper
+    def cropped_image_tag(image)
+      content_tag :picture do
+        Spree::CropperDevice.dimensions.each do |device, dimensions|
+          unless image.blank?
+            content_tag :source, { media: { "max-width": "#{dimensions[:width]}px"}, srcset: main_app.url_for(image.attachment.variant(crop: image.for(device)))}
+          end
+        end
+        unless image.blank?
+          image_tag main_app.url_for(image.attachment.variant(crop: image.for('desktop')))
+        end
+      end
+    end
+  end
+end

af1d7b07430ef38e1f20ce8b92fd3aafb07cd403
artemaminov <artemaminov@gmail.com>
2020-08-25 12:30:22 +0500

    beta 0.2.6.beta

--- lib/spree_cropper/version.rb
+++ lib/spree_cropper/version.rb
@@ -10,7 +10,7 @@ module SpreeCropper
   module VERSION
     MAJOR = 0
     MINOR = 2
-    TINY  = 5
+    TINY  = 6
     PRE   = 'beta'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')

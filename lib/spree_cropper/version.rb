2ef34b554a9a204084a1a6e6ef5edfebad2770ed
artemaminov <artemaminov@gmail.com>
2020-09-08 16:29:37 +0500

    bump 0.3.0.beta

--- lib/spree_cropper/version.rb
+++ lib/spree_cropper/version.rb
@@ -9,8 +9,8 @@ module SpreeCropper
 
   module VERSION
     MAJOR = 0
-    MINOR = 2
-    TINY  = 8
+    MINOR = 3
+    TINY  = 0
     PRE   = 'beta'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')

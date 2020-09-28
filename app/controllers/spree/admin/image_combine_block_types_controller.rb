fea87520e32d9f2a58f86f60b65a8b749e18545f
artemaminov <artemaminov@gmail.com>
2020-09-28 16:12:50 +0300

    build or balance count of cropper_dimensions records

--- app/controllers/spree/admin/image_combine_block_types_controller.rb
+++ app/controllers/spree/admin/image_combine_block_types_controller.rb
@@ -1,6 +1,15 @@
 module Spree
   module Admin
     class ImageCombineBlockTypesController < ResourceController
+      def new
+        @image_combine_block_type.cropper_dimensions = Spree::CropperDimension.all
+      end
+
+      def edit
+        if @image_combine_block_type.cropper_dimensions.count < Spree::CropperDimension.all.count
+          image_combine_croppers = @image_combine_block_type.cropper_dimensions
+          spree_croppers = Spree::CropperDimension.all
+          @image_combine_block_type.cropper_dimensions << spree_croppers - image_combine_croppers
         end
       end
     end

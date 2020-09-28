509830c05be1c7d5199179d5f74b9f681806334c
artemaminov <artemaminov@gmail.com>
2020-09-28 16:17:44 +0300

    add show.js action

--- app/controllers/spree/admin/image_combines_controller.rb
+++ app/controllers/spree/admin/image_combines_controller.rb
@@ -1,7 +1,11 @@
 module Spree
   module Admin
     class ImageCombinesController < ResourceController
-
+      # def show
+      #   respond_to do |format|
+      #     format.js
+      #   end
+      # end
     end
   end
 end
